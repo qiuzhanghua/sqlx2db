0a4f7ce1651cf46f9dadeedf2f0c1b0c6472bfd6
qiuzhanghua <qiuzhanghua@icloud.com>
2021-01-06 20:04:23 +0800

    upgrade sqlx from 0.3 -> 0.4 version -> 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ async fn main() -> Result<(), sqlx::Error> {
     let pg_url = std::env::var("DATABASE_URL");
     if let Ok(url) = pg_url {
         let version = if url.starts_with("mysql") {
-            use sqlx_core::mysql::MySqlQueryAs;
-            let pool = sqlx::MySqlPool::builder().max_size(5).build(&url).await?;
+            use sqlx::mysql::MySqlPoolOptions;
+            let pool = MySqlPoolOptions::new()
+                .max_connections(5)
+                .connect(&url)
+                .await?;
             let row: (String,) = sqlx::query_as(r#"SELECT version() v"#)
                 .fetch_one(&pool)
                 .await?;
             "MySQL ".to_string() + &row.0
         } else {
-            use sqlx_core::postgres::PgQueryAs;
-            let pool = sqlx::PgPool::builder().max_size(5).build(&url).await?;
+            use sqlx::postgres::PgPoolOptions;
+            let pool = PgPoolOptions::new()
+                .max_connections(5)
+                .connect(&url)
+                .await?;
             let row: (String,) = sqlx::query_as(r#"SELECT version() v"#)
                 .fetch_one(&pool)
                 .await?;
